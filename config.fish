432719c3d55f8310f924f400ab737a71c4f872e4
Sherif Soliman <sherif@ssoliman.com>
2016-11-27 23:17:41 -0500

    Add alias for julia.

--- config.fish
+++ config.fish
@@ -44,6 +44,9 @@ alias caplog='python3 ~/projects/caplog/caplog.py'
 
 alias desktop='bash ~/scripts/desktop.sh'
 
+#julia
+alias julia=' exec /Applications/Julia-0.5.app/Contents/Resources/julia/bin/julia'
+
 #     .                                             
 #   .o8                                             
 # .o888oo ooo. .oo.  .oo.   oooo  oooo  oooo    ooo 
