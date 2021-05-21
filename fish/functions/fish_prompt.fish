29ab3eac18bbf32f7162ad6b298fd25d7c8b5f0a
Sherif Soliman <code@sherif.io>
2021-05-20 23:16:47 -0700

    fish: new colors, pretty prompt, pretty everything.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -4,68 +4,19 @@
 function __ssh_badge
 	if test -n "$SSH_CLIENT$SSH2_CLIENT$SSH_TTY"
 		set_color -b d6aeec -o 2a0a8b
-		echo -n " "(string upper (string sub -s 1 -l 1 (hostname -s)))" "
-		set_color normal
+		echo -n " "(string upper (string sub -s 1 -l 1 (hostname -s)))" "(set_color normal)" "
 	end
 end
 
 function __current_path
 	# Replace HOME with ~
-	set -l path (string replace "$HOME" (set_color a86ec8)"~"(set_color -d white) (pwd))
+	set -l path (string replace "$HOME" (set_color 6d8cb8)"~" (pwd))
 	# Highlight last path element
 	set -l parts (string split "/" $path)
-	set parts[-1] (set_color normal)(set_color -o brwhite)$parts[-1](set_color normal)
+	set parts[-1] (set_color normal)(set_color eca3c5)$parts[-1](set_color normal)
 	set path (string join "/" $parts)
 
-	echo -n " "$path(set_color normal)
-end
-
-function __git_status
-    set git_branch (command git rev-parse --abbrev-ref HEAD 2> /dev/null)
-    set git_tracked (git for-each-ref --format='%(upstream:short)' (command git symbolic-ref -q HEAD 2> /dev/null) 2> /dev/null)
-    set git_stashed (command git rev-parse --verify --quiet refs/stash 2> /dev/null)
-
-    set -l submodule_syntax
-    set submodule_syntax "--ignore-submodules=dirty"
-    set git_dirty (command git status -s $submodule_syntax  2> /dev/null)
-
-    # put dirty branch indicator in prompt
-    set  __fish_git_prompt_char_dirty ＊
-    set  __fish_git_prompt_char_stashed ℎ
-    set  __fish_git_prompt_char_dirty_and_stashed ＊ ℎ
-
-    if [ -n "$git_branch" ]
-        set git_behind (command git rev-list "$git_branch".."$git_tracked" --count 2> /dev/null)
-        set git_ahead (command git rev-list "$git_tracked".."$git_branch" --count 2> /dev/null)
-
-        set git_status "  $git_branch"
-        if [ -n "$git_dirty" ]
-            if [ "$git_stashed" ]
-                set git_full "$git_status$__fish_git_prompt_char_dirty_and_stashed"
-            else
-                set git_full "$git_status"
-            end
-        else
-            if [ "$git_stashed" ]
-                set git_full "$git_status $__fish_git_prompt_char_stashed"
-            else
-                set git_full "$git_status"
-            end
-        end
-
-        if test $git_behind -gt 0
-            set git_full "$git_full $git_behind↓"
-        end
-        if test $git_ahead -gt 0
-            set git_full "$git_full $git_ahead↑"
-        end
-    end
-
-    if [ -n "$git_dirty" ]
-        echo -n (set_color e67e00)$git_full (set_color normal)
-    else
-        echo -n (set_color brgreen)"$git_full "(set_color normal)
-    end
+	echo -n $path(set_color normal)
 end
 
 function fish_prompt
@@ -73,8 +24,7 @@ function fish_prompt
     and echo (set_color red)"#"
 
     echo -e '\r'
-    # Main
     __ssh_badge
     __current_path
-    __git_status
+    echo -e ' '(random choice ◐ ◑ ◒ ◓ ◔ ◕) ' '
 end
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,7 +1,48 @@
 function fish_right_prompt -d "Write out the right prompt with non zero exit status and date/time"
+    __git_status
+
     set nstat $status
     if test $nstat -ne 0
-        echo (set_color red)(set_color eeeeee)(set_color -b red)$nstat(set_color -b normal)(set_color red)(set_color red)
+        echo (set_color red)(set_color eeeeee)(set_color -b red)(fish_status_to_signal $nstat)(set_color -b normal)(set_color red)(set_color red)
     end
-    echo -n (set_color 00a4f7)(set_color eeeeee)(set_color -b 00a4f7)(date +"%H:%M:%S")(set_color -b normal)(set_color 00a4f7)(set_color normal)
+    echo -n (set_color 6d8cb8)(set_color eeeeee)(set_color -b 6d8cb8)(date +"%H:%M:%S")(set_color -b normal)(set_color 6d8cb8)(set_color normal)
+
+end
+
+function __git_status
+    set git_branch (command git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    set git_tracked (git for-each-ref --format='%(upstream:short)' (command git symbolic-ref -q HEAD 2> /dev/null) 2> /dev/null)
+    set git_stashed (command git rev-parse --verify --quiet refs/stash 2> /dev/null)
+
+    set -l submodule_syntax
+    set submodule_syntax "--ignore-submodules=dirty"
+    set git_dirty (command git status -s $submodule_syntax  2> /dev/null)
+
+    set  __fish_git_prompt_char_stashed ℎ
+
+    if [ -n "$git_branch" ]
+        set git_behind (command git rev-list "$git_branch".."$git_tracked" --count 2> /dev/null)
+        set git_ahead (command git rev-list "$git_tracked".."$git_branch" --count 2> /dev/null)
+
+        set git_status " $git_branch"
+        if [ "$git_stashed" ]
+            set git_full "$git_status $__fish_git_prompt_char_stashed"
+        else
+            set git_full "$git_status"
+        end
+
+        if test $git_behind -gt 0
+            set git_full "$git_full $git_behind↓"
+        end
+        if test $git_ahead -gt 0
+            set git_full "$git_full $git_ahead↑"
+        end
+        if [ -n "$git_dirty" ]
+            set branch_color fb9420
+        else
+            set branch_color 29c37b
+
+        end
+        echo -n (set_color $branch_color)(set_color eeeeee)(set_color -b $branch_color)$git_full(set_color -b normal)(set_color $branch_color)(set_color normal)
+    end
 end
