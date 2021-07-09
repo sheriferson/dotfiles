f5dbf243f85681f68b1c9990beb628351afa6e86
Sherif Soliman <code@sherif.io>
2021-07-09 11:41:32 -0700

    fish: improve right prompt bubbles again.

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,10 +1,15 @@
 function fish_right_prompt -d "Write out the right prompt with non zero exit status and date/time"
     set nstat $status
     __git_status
+    t_today
     if test $nstat -ne 0
         print_bubble '⮾ '(fish_status_to_signal $nstat) red eeeeee
     end
     print_bubble (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}') 6d8cb8 eeeeee
+end
+
+function t_today
+    print_bubble ' '(cat ~/mytasks/tasks.txt | grep '@today' | wc -l | sed -e's/ *//') 907193 eeeeee
 end
 
 function __git_status
@@ -16,7 +21,7 @@ function __git_status
     set submodule_syntax "--ignore-submodules=dirty"
     set git_dirty (command git status -s $submodule_syntax  2> /dev/null)
 
-    set  __fish_git_prompt_char_stashed ℎ
+    set  __fish_git_prompt_char_stashed ' '
 
     if [ -n "$git_branch" ]
         set git_behind (command git rev-list "$git_branch".."$git_tracked" --count 2> /dev/null)
@@ -31,16 +36,16 @@ function __git_status
 
         if [ "$git_branch" != "HEAD" ]
             if test $git_behind -gt 0
-                set git_full "$git_full $git_behind↓"
+                set git_full "$git_full  $git_behind"
             end
             if test $git_ahead -gt 0
-                set git_full "$git_full $git_ahead↑"
+                set git_full "$git_full  $git_ahead"
             end
         end
         if [ -n "$git_dirty" ]
-            set branch_color fb9420
+            set branch_color fb9420  # dirty, orange
         else
-            set branch_color 29c37b
+            set branch_color 29c37b  # clean, green
 
         end
         print_bubble $git_full $branch_color eeeeee
