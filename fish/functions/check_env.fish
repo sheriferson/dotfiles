62fa165a82859317a30805f69a14f3f9780c9e3c
Sherif Soliman <code@sherif.io>
2025-01-21 02:00:41 -0800

    fish: Update check_env.fish

--- fish/functions/check_env.fish
+++ fish/functions/check_env.fish
@@ -1,7 +1,11 @@
-function check_env -d "Check if the string prod appears in $ENV_BASE and add a warning segment if so"
-    if [  (string match -r '.*prod.*' $ENV_BASE) ]
-        echo " "(print_bubble "  "$ENV_BASE $fish_color_error f5ddea)" "
+function check_env -d "Check if $ENV_BASE is set, if the string 'prod' appears in $ENV_BASE and add a warning segment if so"
+    if test -n "$ENV_BASE"
+        if [  (string match -r '.*prod.*' $ENV_BASE) ]
+            echo " "(print_bubble "  "$ENV_BASE $fish_color_error f5ddea)" "
+        else
+            echo " "(print_bubble "󰓽  "$ENV_BASE $fish_color_normal dae2f4)" "
+        end
     else
-        echo " "(print_bubble "󰓽  "$ENV_BASE $fish_color_normal dae2f4)" "
+        echo " "
     end
 end
