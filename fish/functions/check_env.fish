bf0bf6d39bae5ccc473b14a70d520afaec2928d8
Sherif Soliman <code@sherif.io>
2025-07-09 18:31:11 -0700

    One more improvement to fish prompt weird spacing.

--- fish/functions/check_env.fish
+++ fish/functions/check_env.fish
@@ -1,9 +1,9 @@
 function check_env -d "Check if $ENV_BASE is set, if the string 'prod' appears in $ENV_BASE and add a warning segment if so"
     if test -n "$ENV_BASE"
         if [  (string match -r '.*prod.*' $ENV_BASE) ]
-            echo " "(print_bubble "  "$ENV_BASE $fish_color_error f5ddea)" "
+            echo (print_bubble "  "$ENV_BASE $fish_color_normal ff8861)
         else
-            echo " "(print_bubble "󰓽  "$ENV_BASE $fish_color_normal dae2f4)" "
+            echo (print_bubble "󰓽  "$ENV_BASE $fish_color_normal dae2f4)
         end
     else
         echo " "
