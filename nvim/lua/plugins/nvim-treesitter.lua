ca8f15fdece78e5bcb9a6eaebb26792d38b25a23
Sherif Soliman <code@sherif.io>
2026-08-13 17:10:53 -0700

    nvim: turns out nvim-treesitter install has changed.

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,32 +1,8 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        lazy = false,
         build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = { "c",
-                                     "bash",
-                                     "elixir",
-                                     "heex",
-                                     "html",
-                                     "javascript",
-                                     "lua",
-                                     "python",
-                                     "query",
-                                     "regex",
-                                     "vim",
-                                     "vimdoc",
-                },
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                    disable = { "c", "rust", "latex", "tex" },
-                },
-                indent = {
-                    enable = true
-                },
-            })
-        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects"
